55f4ee1625494c3d761e972fa7185221a8b53c9d
Michael Pollmeier <michael@michaelpollmeier.com>
2016-11-22 16:32:33 +1300

    refactor: separate toMap and fromMap into namespaces

--- macros/src/main/scala/mp/entity.scala
+++ macros/src/main/scala/mp/entity.scala
@@ -9,39 +9,43 @@ import scala.meta._
 class entity extends StaticAnnotation {
   inline def apply(defn: Any): Any = meta {
     val q"..$mods class $tname[..$tparams] ..$ctorMods (...$paramss) extends $template" = defn
-
-    val paramssFlat: Seq[Term.Param] = paramss.flatten
-    def toMapContents(entityName: Term.Name): Seq[Term] = paramssFlat.map { param =>
-      val memberName = Term.Name(param.name.value)
-      q"${param.name.value} -> $entityName.$memberName"
-    }
-
-    // TODO low prio: support multiple constructor params lists
-    val ctorParamsFirst: Seq[Term.Param] = paramss.headOption.getOrElse(Nil)
-
-    def ctorArgs(valuesName: Term.Name): Seq[Term] = ctorParamsFirst.map { param =>
-      val nameTerm = Term.Name(param.name.value)
-      val tpe: Type = param.decltpe.get.asInstanceOf[Type.Name] // TODO: don't do option.get, don't cast
-      q""" $nameTerm = $valuesName(${param.name.value}).asInstanceOf[$tpe] """
-    }
-
     val typeTermName = Term.Name(tname.value)
-    val fromMapCtorValuesName: Term.Name = q"values"
-    val toMapEntityName: Term.Name = q"entity"
+
+    object ToMap {
+      val entityName: Term.Name = q"entity"
+      val paramssFlat: Seq[Term.Param] = paramss.flatten
+      def keyValues(entityName: Term.Name): Seq[Term] = paramssFlat.map { param =>
+        val memberName = Term.Name(param.name.value)
+        q"${param.name.value} -> $entityName.$memberName"
+      }
+    }
+
+    object FromMap {
+      val ctorValuesName: Term.Name = q"values"
+
+    // TODO: support multiple constructor params lists
+      val ctorParamsFirst: Seq[Term.Param] = paramss.headOption.getOrElse(Nil)
+      def ctorArgs(valuesName: Term.Name): Seq[Term] = ctorParamsFirst.map { param =>
+        val nameTerm = Term.Name(param.name.value)
+        val tpe: Type = param.decltpe.get.asInstanceOf[Type.Name] // TODO: don't do option.get, don't cast
+        q""" $nameTerm = $valuesName(${param.name.value}).asInstanceOf[$tpe] """
+      }
+
+    }
     val res = q"""
       ..$mods class $tname[..$tparams](...$paramss) {
       }
 
       object $typeTermName {
-        def fromMap(values: Map[String, Any]): $tname =
-          ${typeTermName}(..${ctorArgs(fromMapCtorValuesName)})
+        def toMap(${ToMap.entityName}: $tname): Map[String, Any] =
+          Map[String, Any](..${ToMap.keyValues(ToMap.entityName)})
 
-        def toMap($toMapEntityName: $tname): Map[String, Any] =
-          Map[String, Any](..${toMapContents(toMapEntityName)})
+        def fromMap(values: Map[String, Any]): $tname =
+          ${typeTermName}(..${FromMap.ctorArgs(FromMap.ctorValuesName)})
       }
     """
 
-    println(res)
+    // println(res)
     res
   }
 }
