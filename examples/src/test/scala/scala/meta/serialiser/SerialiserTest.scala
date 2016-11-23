1feeaf4d3dc48132df251e1d1834463b02444ea1
Michael Pollmeier <michael@michaelpollmeier.com>
2016-11-23 14:40:25 +1300

    simplify tests

--- examples/src/test/scala/scala/meta/serialiser/SerialiserTest.scala
+++ examples/src/test/scala/scala/meta/serialiser/SerialiserTest.scala
@@ -11,30 +11,18 @@ class SerialiserTest extends WordSpec with Matchers {
   import TestEntities._
 
   "simple case class" should {
-    "serialise to Map" in {
+    "serialise and deserialise" in {
       val testInstance = SimpleCaseClass(i = 42, s = "something")
       val keyValues = SimpleCaseClass.toMap(testInstance)
-      keyValues should contain ("i" -> testInstance.i)
-      keyValues should contain ("s" -> testInstance.s)
-    }
-
-    "deserialise from Map" in {
-      val testInstance = SimpleCaseClass.fromMap(Map("i" -> 42, "s" -> "something"))
-      testInstance.i shouldBe 42
-      testInstance.s shouldBe "something"
+      SimpleCaseClass.fromMap(keyValues) shouldBe testInstance
     }
   }
 
   "case class with type param" should {
-    "serialise to Map" in {
+    "serialise and deserialise" in {
       val testInstance = WithTypeParam[Integer](n = 43)
       val keyValues = WithTypeParam.toMap(testInstance)
-      keyValues should contain ("n" -> testInstance.n)
-    }
-
-    "deserialise from Map" in {
-      val testInstance = WithTypeParam.fromMap(Map("n" -> 43))
-      testInstance.n shouldBe 43
+      WithTypeParam.fromMap(keyValues) shouldBe testInstance
     }
   }
 
