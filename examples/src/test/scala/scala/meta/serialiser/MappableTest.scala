18a29d9bb7f6e4409f698954666129e98d7b35b0
Michael Pollmeier <michael@michaelpollmeier.com>
2016-11-30 19:39:56 +1300

    proper support for existing companion object: keep existing statements

--- examples/src/test/scala/scala/meta/serialiser/MappableTest.scala
+++ examples/src/test/scala/scala/meta/serialiser/MappableTest.scala
@@ -3,59 +3,63 @@ package scala.meta.serialiser
 import org.scalatest._
 
 object TestEntities {
-  // @mappable case class SimpleCaseClass(i: Int, s: String)
-  // @mappable case class WithTypeParam[N <: Number](n: Number)
-  // @mappable case class WithBody(i: Int) { def banana: Int = i }
+  @mappable case class SimpleCaseClass(i: Int, s: String)
+  @mappable case class WithTypeParam[N <: Number](n: Number)
+  @mappable case class WithBody(i: Int) { def banana: Int = i }
 
-  object CaseClassWithCompanion { def existingFun(): Int = 42 }
-  @mappable case class CaseClassWithCompanion (i: Int, s: String)
+  object WithCompanion { def existingFun(): Int = 42 }
+  @mappable case class WithCompanion (i: Int, s: String)
 }
 
 class MappableTest extends WordSpec with Matchers {
   import TestEntities._
 
-  // "simple case class" should {
-  //   "serialise and deserialise" in {
-  //     val testInstance = SimpleCaseClass(i = 42, s = "something")
-  //     val keyValues = SimpleCaseClass.toMap(testInstance)
-  //     SimpleCaseClass.fromMap(keyValues) shouldBe Some(testInstance)
-  //   }
-  // }
+  "simple case class" should {
+    "serialise and deserialise" in {
+      val testInstance = SimpleCaseClass(i = 42, s = "something")
+      val keyValues = SimpleCaseClass.toMap(testInstance)
+      SimpleCaseClass.fromMap(keyValues) shouldBe Some(testInstance)
+    }
+  }
 
-  // "case class with type param" should {
-  //   "serialise and deserialise" in {
-  //     val testInstance = WithTypeParam[Integer](n = 43)
-  //     val keyValues = WithTypeParam.toMap(testInstance)
-  //     WithTypeParam.fromMap(keyValues) shouldBe Some(testInstance)
-  //   }
-  // }
+  "case class with type param" should {
+    "serialise and deserialise" in {
+      val testInstance = WithTypeParam[Integer](n = 43)
+      val keyValues = WithTypeParam.toMap(testInstance)
+      WithTypeParam.fromMap(keyValues) shouldBe Some(testInstance)
+    }
+  }
 
-  // "case class with body" should {
-  //   "still have the body as before" in {
-  //     WithBody(100).banana shouldBe 100
-  //   }
-  // }
+  "case class with body" should {
+    "still have the body as before" in {
+      WithBody(100).banana shouldBe 100
+    }
+  }
 
   "case class with companion" should {
     "serialise and deserialise" in {
-      val testInstance = CaseClassWithCompanion(i = 42, s = "something")
-      val keyValues = CaseClassWithCompanion.toMap(testInstance)
-      CaseClassWithCompanion.fromMap(keyValues) shouldBe Some(testInstance)
+      val testInstance = WithCompanion(i = 42, s = "something")
+      val keyValues = WithCompanion.toMap(testInstance)
+      WithCompanion.fromMap(keyValues) shouldBe Some(testInstance)
     }
 
-    // "maintain existing code in companion" in {
-    //   CaseClassWithCompanion.existingFun shouldBe 42
-    // }
+    "keep existing functionality in companion" in {
+      WithCompanion.existingFun shouldBe 42
+    }
   }
 
-  // "fromMap" should {
-  //   "return None if provided with invalid data" in {
-  //     val invalidKeyValues = Map("in" -> "valid")
+  "fromMap" should {
+    "return None if provided with invalid data" in {
+      val invalidKeyValues = Map("in" -> "valid")
 
-  //     Seq(SimpleCaseClass.fromMap _, WithTypeParam.fromMap _, WithBody.fromMap _) foreach { fromMap =>
-  //       fromMap(invalidKeyValues) shouldBe None
-  //     }
-  //   }
-  // }
+      Seq(
+        SimpleCaseClass.fromMap _,
+        WithTypeParam.fromMap _,
+        WithBody.fromMap _,
+        WithCompanion.fromMap _) foreach { fromMap =>
+        fromMap(invalidKeyValues) shouldBe None
+      }
+    }
+  }
 
 }
--- src/main/scala/scala/meta/serialiser/mappable.scala
+++ src/main/scala/scala/meta/serialiser/mappable.scala
@@ -9,11 +9,17 @@ import scala.meta._
 class mappable extends StaticAnnotation {
   inline def apply(defn: Any): Any = meta {
 
-    // defined class may or may not have a companion object...
+    // defined class may or may not have a companion object
     val (classDefn: Defn.Class, compDefnOption: Option[Defn.Object]) = defn match {
       case classDefn: Defn.Class => (classDefn, None) //only class
       case Term.Block((classDefn: Defn.Class) :: (compDefn: Defn.Object) :: Nil) => (classDefn, Option(compDefn)) // class + companion
       case _ => abort(defn.pos, "Invalid annottee")
+    }
+
+    // get existing companion object statements (if any)
+    val compStats: Seq[Stat] = compDefnOption match {
+      case None => Nil
+      case Some(compDefn) => compDefn.templ.stats.getOrElse(Nil)
     }
 
     val q"..$mods class $tName[..$tParams] ..$ctorMods (...$paramss) extends $template" = classDefn
@@ -59,11 +65,12 @@ class mappable extends StaticAnnotation {
           scala.util.Try {
             ${tCompleteTerm}(..${FromMap.ctorArgs(FromMap.ctorValuesName)})
           }.toOption
+
+         ..$compStats
       }
     """
 
-
-    println(res)
+    // println(res)
     res
   }
 }
